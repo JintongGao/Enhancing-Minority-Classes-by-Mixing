af32c704b124fb9fe993f4c941ccdcc353d341cf
gaojt20 <128790241+JintongGao@users.noreply.github.com>
2023-10-15 00:24:09 +0800

    Add files via upload

---
+++ imbalance_data/imbalance_cifar.py
@@ -0,0 +1,121 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from PIL import Image
+import random
+
+
+class IMBALANCECIFAR10(torchvision.datasets.CIFAR10):
+    cls_num = 10
+
+    def __init__(self, root, imb_type='exp', imb_factor=0.01, rand_number=0, weighted_alpha=1,
+                 train=True, transform=None, target_transform=None, download=False):
+        super(IMBALANCECIFAR10, self).__init__(root, train, transform, target_transform, download)
+        np.random.seed(rand_number)
+        self.weighted_alpha = weighted_alpha
+        img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
+        self.gen_imbalanced_data(img_num_list)
+
+    def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
+        img_max = len(self.data) / cls_num
+        img_num_per_cls = []
+        if imb_type == 'exp':
+            for cls_idx in range(cls_num):
+                num = img_max * (imb_factor ** (cls_idx / (cls_num - 1.0)))
+                img_num_per_cls.append(int(num))
+        else:
+            img_num_per_cls.extend([int(img_max)] * cls_num)
+        return img_num_per_cls
+
+    def gen_imbalanced_data(self, img_num_per_cls):
+        new_data = []
+        new_targets = []
+        targets_np = np.array(self.targets, dtype=np.int64)
+        classes = np.unique(targets_np)
+
+        self.num_per_cls_dict = dict()
+        for the_class, the_img_num in zip(classes, img_num_per_cls):
+            self.num_per_cls_dict[the_class] = the_img_num
+            idx = np.where(targets_np == the_class)[0]
+            np.random.shuffle(idx)
+            selec_idx = idx[:the_img_num]
+            
+            #print(selec_idx)
+            new_data.append(self.data[selec_idx, ...])
+            new_targets.extend([the_class, ] * the_img_num)
+        new_data = np.vstack(new_data)
+        self.data = new_data
+        self.targets = new_targets
+
+    def get_cls_num_list(self):
+        cls_num_list = []
+        for i in range(self.cls_num):
+            cls_num_list.append(self.num_per_cls_dict[i])
+        return cls_num_list
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
+
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+                
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def get_weighted_sampler(self):
+        cls_num_list = self.get_cls_num_list()
+        cls_weight = 1.0 / (np.array(cls_num_list) ** self.weighted_alpha)
+        cls_weight = cls_weight / np.sum(cls_weight) * len(cls_num_list)
+        print("cls_weight", cls_weight)
+        samples_weight = np.array([cls_weight[t] for t in self.targets])
+        samples_weight = torch.from_numpy(samples_weight)
+        samples_weight = samples_weight.double()
+        print("samples_weight", samples_weight)
+        sampler = torch.utils.data.WeightedRandomSampler(samples_weight, len(self.targets), replacement=True)
+        return sampler
+
+
+class IMBALANCECIFAR100(IMBALANCECIFAR10):
+
+    base_folder = 'cifar-100-python'
+    url = "https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz"
+    filename = "cifar-100-python.tar.gz"
+    tgz_md5 = 'eb9058c3a382ffc7106e4002c42a8d85'
+    train_list = [
+        ['train', '16019d7e3df5f24257cddd939b257f8d'],
+    ]
+
+    test_list = [
+        ['test', 'f0ef6b0ae62326f3e7ffdfab6717acfc'],
+    ]
+    meta = {
+        'filename': 'meta',
+        'key': 'fine_label_names',
+        'md5': '7973b15100ade9c7d40fb424638fde48',
+    }
+    cls_num = 100
+
+
+if __name__ == '__main__':
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trainset = IMBALANCECIFAR10(root='./data', train=True,
+                                 download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(
+            trainset, batch_size=256, shuffle=False,
+            num_workers=4, pin_memory=True)
+    for i, (data, label) in enumerate(train_loader):
+        print(label)
